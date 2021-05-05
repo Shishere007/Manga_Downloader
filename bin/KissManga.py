49fc145830669f8a2ab2e191fe1eca1892a2b603
Shishere007 <55714591+Shishere007@users.noreply.github.com>
2021-05-05 17:14:48 +0530

    KissManga.py finished. gui pending

--- bin/KissManga.py
+++ bin/KissManga.py
@@ -1,23 +1,57 @@
 from bin import tools
-
+import concurrent.futures
+import multiprocessing
 
 __author__ = "Shishere"
 __version__ = "1.0"
 
 
 class KissManga:
-    def __init__(self,manga_name:str,manga_link:str) -> None:
+    def __init__(self, manga_name: str) -> None:
         self.__base_url = 'https://kissmanga.org'
+        # self.__search = f"https://kissmanga.org/manga_list?q={self.manga_name.replace(' ','+')}&action=search"
+
         self.manga_name = manga_name
-        self.manga_link = manga_link
+        self.manga_link = ''
 
         self.__chapter_list_main_div_className = 'listing listing8515 full'
+        self.__pages_div_id = 'centerDivVideo'
+        # self.__search_manga_div_className = 'item_movies_in_cat '
+        # self.__search_manga_pager_className = 'pager'
 
+        self.chapter_list = []
 
-    def start(self,):
-        chapter_list = self.get_chapter_list()
+    # def search(self) -> list:
+    #     '''
+    #     return (list)   :   [{'mange_name':'name','manga_link',:'link','status':'status'}]
+    #     '''
+    #     soup = tools.prepare_soup(link=self.__search)
+    #     if not soup:
+    #         return False
+    #     search_list = []
+    #     def update_manga_list(soup:BeautifulSoup)->None:
+    #         mangas = soup.find_all('div',{'class':self.__search_manga_div_className})
+    #         for manga in mangas:
+    #             div_list = manga.findchildren()
+    #             manga_name = tools.clear_name(div_list[0].text.strip())
+    #             manga_link = self.__base_url + manga.find_all('a')[0].attrs['href']
+    #             status = tools.clear_name(div_list[1].text.strip())
+    #             search_list.append(
+    #                 {
+    #                     'manga_name':manga_name,
+    #                     'manga_link':manga_link,
+    #                     'status':status
+    #                 }
+    #             )
+    #     update_manga_list(soup)
+    #     pager = soup.find_all('ul',{'class':self.__search_manga_pager_className})[0].findAll('li')[1:]
+    #     for page in pager:
+    #         soup = tools.prepare_soup(link=self.__base_url+page.find_all('a')[0].attrs['href'])
+    #         update_manga_list(soup)
 
-    def get_chapter_list(self) -> list:
+    #     return search_list
+
+    def get_chapter_list(self, manga_link: str) -> list:
         '''
         return [
             {
@@ -26,40 +60,70 @@ class KissManga:
             }
         ]
         '''
-        # soup = tools.prepare_soup(link=self.manga_link)
-        soup = tools.test_soup()
+        self.manga_link = manga_link
+        soup = tools.prepare_soup(link=manga_link)
+        # soup = tools.test_soup()
         if not soup:
             return False
-        chapter_div_list = soup.find_all('a')
-        chapter_list = []
+        chapter_div_list = soup.find_all(
+            'div', {'class': self.__chapter_list_main_div_className})[0].find_all('a')
         for a in chapter_div_list[::-1]:
             if 'title' in a.attrs:
                 if a.attrs['title'].__contains__('Read'):
-                    chapter_list.append(
+                    self.chapter_list.append(
                         {
-                            'chapter': tools.clear_chapter_name(a.text.strip()),
+                            'chapter': tools.clear_name(a.text.strip()),
                             'link': self.__base_url + a.attrs['href']
                         }
                     )
-        return chapter_list
+        return self.chapter_list
 
-    def download_chapter(self, chapter_link: str) -> None:
+    def download_chapter(self, chapter: dict) -> None:
+        '''
+        @param
+        chapter (dict)  :   {"chapter":"chapter","link":"link"}
+        '''
+
+        chapter_link = chapter.get('link')
         soup = tools.prepare_soup(link=chapter_link)
         if not soup:
             return False
-        page_list = [item.attrs['src'] for item in soup.find(id='centerDivVideo').find_all('img')]
-        chapter = chapter_link.split("/")[-1].capitalize()
-        
-        tools.create_folder(self.manga_name)
 
+        chapter = chapter_link.split("/")[-1].capitalize().replace("_", " ")
+        tools.create_folder(self.manga_name)
         chapter_folder = self.manga_name + "/" + chapter + "/"
         tools.create_folder(chapter_folder)
 
+        page_list = [item.attrs['src']
+                     for item in soup.find(id=self.__pages_div_id).find_all('img')]
+        image_list = []
+        for ind, url in enumerate(page_list):
+            extension = url.split(".")[-1]
+            page = str(ind+1)
+            if len(page) == 1:
+                page = "0" + page
+            filename = chapter_folder + page + "." + extension
+            image_list.append((filename, url))
+        tools.download_chapter(current_chapter=chapter, image_list=image_list)
         chapter_folder = chapter_folder[:-1]
 
-        
+    def download_multiple_chapters(self, chapter_list: list) -> None:
+        '''
+        @param
+        chapter_list    (list)  : [{"chapter":"chapter","link":"link"}]
+        '''
+        if len(self.chapter_list) == 0:
+            print("NO CHAPTER FOUND")
+            return
+        POOL_SIZE = multiprocessing.cpu_count()
+        with concurrent.futures.ThreadPoolExecutor(max_workers=POOL_SIZE) as executor:
+            executor.map(self.download_chapter, chapter_list)
 
-
+    def download_all_chapters(self,) -> None:
+        if len(self.chapter_list) == 0:
+            print("NO CHAPTER FOUND")
+            return
+        self.download_multiple_chapters(chapter_list=self.chapter_list)
 
 
 if __name__ == "__main__":
--- downloader.py
+++ downloader.py
@@ -1,134 +1,27 @@
+from tkinter import Button,Entry,Label,StringVar, Tk,ttk
+
+
+class downloader:
+    def __init__(self) -> None:
+        pass
+
+    def initialize(self,) -> None:
+        self.__root = Tk()
+        self.__root.title("Manga Downloader")
+        self.__root.minsize(900, 800)
+        self.__root.maxsize(900, 800)
+
+        
+        self._mange_name_link_label = Label(self.__root,text="Manga Link",font=15)
+        self._mange_name_link_label.place(x=10,y=20)
+        self._mange_name_link_textbox = Entry(self.__root,font=15,width=80)
+        self._mange_name_link_textbox.place(x=110,y=20)
+
+
+        self.__root.mainloop()
 
 
 
 
-
-
-# def download_chapter_pages(link_list, chapter, folder):
-#     image_list = []
-#     cnt = len(link_list)
-#     for ind, url in enumerate(link_list):
-#         ext = url.split(".")[-1]
-#         page = str(ind + 1)
-#         if len(page) == 1:
-#             page = "0" + page
-#         filename = folder + page + "." + ext
-#         image_list.append((filename, url, ind+1, cnt))
-#     print(f"Downloading {chapter}", end=" -> ")
-#     thread_download(image_list)
-#     print("\n")
-
-
-def download_chapter(link, manga_name):
-    soup = prepare_soup(link)
-    img_list_temp = [item.attrs['src']
-                     for item in soup.find(id='centerDivVideo').find_all('img')]
-    # manga_name = format_manga_name(link.split("/")[-2])
-    chapter = link.split("/")[-1].capitalize()
-    chapter = manga_name.replace(" ", "_") + "_" + chapter
-    create_folder(manga_name)
-    folder = manga_name + "/" + chapter + "/"
-    create_folder(folder)
-    download_chapter_pages(img_list_temp, chapter, folder)
-    folder = folder[:-1]
-#     # system(f"7z a -tzip {folder}.zip {folder}")
-
-
-
-
-
-# if __name__ == "__main__":
-#     # link = 'https://kissmanga.org/chapter/read_horimiya_manga/chapter_27'
-#     # download_chapter(link)
-#     base = 'https://kissmanga.org'
-
-#     # manga_name = "Horimiya"
-#     manga_name = "Bokura Wa Minna Kawaisou"
-
-#     div = ''
-#     soup = BeautifulSoup(div, 'html.parser')
-
-#     # div_list = soup.find_all('a',{'title':'Read Horimiya Manga online '})
-#     div_list = soup.find_all(
-#         'a', {'title': 'Read Bokura Wa Minna Kawaisou Manga online '})
-#     link_list = [base+a.attrs['href'] for a in div_list[::-1]]
-#     print(len(link_list))
-#     for link in link_list[75:80]:
-#         print(link)
-#         download_chapter(link, manga_name)
-
-
-
-import zipfile
-from bin.KissManga import KissManga
-
-# for i in KissManga().get_chapter_list('https://kissmanga.org/manga/yw925560'):
-#     print(i)
-
-import shutil
-
-from os import path
-
-# src = path.realpath(r'test\new file')
-# print(src)
-
-# shutil.make_archive(r'new file',"zip",src)
-
-# from zipfile import  ZipFile
-
-# a = ZipFile('test/new file.zip','w')
-# a.close()
-
-
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
-    # Print New Line on Complete
-    if iteration == total: 
-        print()
-
-import time
-
-# A List of Items
-items = list(range(0, 57))
-item_len = len(items)
-
-# Initial call to print 0% progress
-printProgressBar(iteration=0, total=item_len, prefix = 'Progress:', suffix = 'Complete', length = 100)
-for i, item in enumerate(items):
-    # Do stuff...
-    time.sleep(0.1)
-    # Update Progress Bar
-    printProgressBar(i + 1, total=item_len, prefix = 'Progress:', suffix = 'Complete', length = 100)
-
-
-printProgressBar(iteration=0, total=item_len, prefix = 'Progress:', suffix = 'Complete', length = 100)
-for i, item in enumerate(items):
-    # Do stuff...
-    time.sleep(0.1)
-    # Update Progress Bar
-    printProgressBar(i + 1, total=item_len, prefix = 'Progress:', suffix = 'Complete', length = 100)
-
-
-printProgressBar(iteration=0, total=item_len, prefix = 'Progress:', suffix = 'Complete', length = 100)
-for i, item in enumerate(items):
-    # Do stuff...
-    time.sleep(0.1)
-    # Update Progress Bar
-    printProgressBar(i + 1, total=item_len, prefix = 'Progress:', suffix = 'Complete', length = 100)
-
-
+if __name__ == "__main__":
+    downloader().initialize()
